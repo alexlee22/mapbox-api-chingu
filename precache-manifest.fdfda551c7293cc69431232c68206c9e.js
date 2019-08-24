8f75f5073cb6a4add6775008e4bda737716c6cf3
Alexander Lee <10814755+alexlee22@users.noreply.github.com>
2019-08-25 02:54:05 +1000

    Updates

---
+++ precache-manifest.fdfda551c7293cc69431232c68206c9e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed26b865d9482adb8e55aee49fc09c7a",
+    "url": "/mapbox-api-chingu/index.html"
+  },
+  {
+    "revision": "354cab48410daf76b3a2",
+    "url": "/mapbox-api-chingu/static/js/2.038a5136.chunk.js"
+  },
+  {
+    "revision": "f7e85767078d8effecf7",
+    "url": "/mapbox-api-chingu/static/js/main.e198b3c6.chunk.js"
+  },
+  {
+    "revision": "4eec55bbce7c555491d0",
+    "url": "/mapbox-api-chingu/static/js/runtime~main.cc882392.js"
+  }
+]);
